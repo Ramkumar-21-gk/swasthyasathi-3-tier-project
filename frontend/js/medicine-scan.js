2dced668260711112fa4029c8ff57c66634e8fee
Ramkumar-21-gk <gkramkumarprajapati49@gmail.com>
2026-02-20 20:27:10 +0530

    all set to go

--- frontend/js/medicine-scan.js
+++ frontend/js/medicine-scan.js
@@ -29,7 +29,7 @@ async function handleImageUpload(event) {
     showMedicineDetails();
   } catch (err) {
     console.error(err);
-    alert("OCR failed. Try aga  in.");
+    alert("OCR failed. Try again.");
     searchBox.value = "";
   }
 }
